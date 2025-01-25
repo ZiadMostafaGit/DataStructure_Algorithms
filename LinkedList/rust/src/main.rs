e18ba1be89e46c859d34056a3ae5b01eee8881ca
ziad_mostafa <ziadmostafa760@gmail.com>
2025-01-25 18:39:46 +0200

    add some functions just some pain to just iterate throw LinkedList but ok and ignore the target dir no need for it

--- LinkedList/rust/src/main.rs
+++ LinkedList/rust/src/main.rs
@@ -1,6 +1,7 @@
 use std::cell::{Ref, RefCell};
 use std::io::Cursor;
 use std::rc::Rc;
+use std::thread::current;
 
 struct Node {
     val: i32,
@@ -62,6 +63,7 @@ impl LinkedList {
         }
         if let Some(ref head) = self.head.clone() {
             if head.borrow().next.is_none() {
+                self.length -= 1;
                 self.head = None;
                 self.tail = None;
                 return;
@@ -76,12 +78,105 @@ impl LinkedList {
                     node.borrow_mut().next = None;
 
                     self.tail = Some(node.clone());
+                    self.length -= 1;
                     return;
                 }
             }
 
             current = next_node;
         }
+    }
+
+    fn pop_front(&mut self) {
+        if self.head.is_none() {
+            return;
+        }
+
+        if let Some(ref head) = self.head.clone() {
+            if head.borrow().next.is_none() {
+                self.head = None;
+                self.tail = None;
+                self.length -= 1;
+                return;
+            }
+        }
+
+        if let Some(ref next) = self.head.clone() {
+            self.length -= 1;
+            self.head = next.borrow_mut().next.clone();
+            return;
+        }
+    }
+
+    fn swap_head_and_tail(&mut self) {
+        let mut current = self.head.clone();
+        let temp = self.head.as_ref().unwrap().borrow().next.clone();
+
+        while let Some(ref node) = current.clone() {
+            let next_node = node.borrow_mut().next.clone();
+
+            if let Some(ref next) = next_node {
+                if next.borrow().next.is_none() {
+                    node.borrow_mut().next = self.head.clone();
+                    next.borrow_mut().next = temp.clone();
+                    let head = self.head.clone().unwrap();
+                    self.head = self.tail.clone();
+                    self.tail = Some(head);
+                    self.tail.as_ref().unwrap().borrow_mut().next = None;
+                    break;
+                } else {
+                    current = node.borrow_mut().next.clone();
+                }
+            } else {
+                return;
+            }
+        }
+    }
+
+    fn left_rotate(&mut self, k: i32) {
+        if k <= 0 {
+            return;
+        }
+        let mut iter = k % self.length;
+
+        if iter == 0 {
+            return;
+        }
+
+        let next = self.tail.as_ref().unwrap().clone();
+        next.borrow_mut().next = self.head.clone();
+
+        let mut current = self.head.clone();
+        while let Some(ref node) = current.clone() {
+            if iter == 1 {
+                break;
+            }
+            iter -= 1;
+            current = node.borrow_mut().next.clone();
+        }
+
+        self.head = current.as_ref().unwrap().borrow().next.clone();
+        self.tail = current.clone();
+        if let Some(ref node) = current {
+            node.borrow_mut().next = None;
+        }
+    }
+
+    fn get_n_from_back(&mut self, index: i32) -> Option<i32> {
+        if index < 0 || index > self.length {
+            return None;
+        }
+        let iter = self.length.clone() - index;
+        let mut current = self.head.clone();
+        for _ in 0..iter {
+            if let Some(ref node) = current.clone() {
+                current = node.borrow().next.clone();
+            } else {
+                return None;
+            }
+        }
+
+        current.map(|node| node.borrow().val)
     }
 
     fn helper(&self, current: Option<Rc<RefCell<Node>>>) {
@@ -105,8 +200,6 @@ impl LinkedList {
 }
 
 fn main() {
-    println!("Hello, world!");
-
     let mut list = LinkedList::new();
     list.push_back(1);
     list.push_back(2);
@@ -117,12 +210,25 @@ fn main() {
     // list.print();
     // list.push_front(7);
 
-    list.print();
+    // list.print();
+    // list.swap_head_and_tail();
+    // list.left_rotate(10);
+    // list.print();
     // list.print_rev();
     // list.print();
-
-    list.pop_back();
-    list.pop_back();
-
-    list.print();
+    //
+    // println!("=======================");
+    // let res = list.get_n_from_back(7);
+    // match res {
+    //     Some(num) => {
+    //         println!("{}", num);
+    //     }
+    //     None => {
+    //         println!("Error None");
+    //     }
+    // }
+    // list.pop_back();
+    //
+    // list.pop_front();
+    // list.print();
 }
