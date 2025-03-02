e1703308ef5027664ffa30d4eac3244d1ab363f9
ziad_mostafa <ziadmostafa760@gmail.com>
2025-03-02 13:44:42 +0200

    make things right remove shitty c++ code and add more shitty rust code

---
+++ double_linked_list/target/debug/deps/double_linked_list-7269f9fdd0eebed5.d
@@ -0,0 +1,5 @@
+/home/ziad/git/DSA_deploma/double_linked_list/target/debug/deps/libdouble_linked_list-7269f9fdd0eebed5.rmeta: src/main.rs
+
+/home/ziad/git/DSA_deploma/double_linked_list/target/debug/deps/double_linked_list-7269f9fdd0eebed5.d: src/main.rs
+
+src/main.rs:
